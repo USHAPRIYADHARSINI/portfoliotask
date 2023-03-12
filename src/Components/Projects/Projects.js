5939f3de61a5c7c7ed34714bbac4f81c83c041d3
USHAPRIYADHARSINI <110903183+USHAPRIYADHARSINI@users.noreply.github.com>
2023-03-12 21:40:20 +0530

    anchor tag

--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -6,33 +6,33 @@ function Projects() {
     <div className='projects'>
         <h3>Project Links</h3>
         <div className='projects-container'>
-          <button className="links" variant="text" href="https://spectacular-stroopwafel-e2df72.netlify.app/">
+          <a className="links" variant="text" href="https://spectacular-stroopwafel-e2df72.netlify.app/">
           webcode 1 - Makeup api
-          </button>
+          </a>
           <p>•</p>
-          <button className="links" variant="text" href="https://chatapp-client-usha.netlify.app">
+          <a className="links" variant="text" href="https://chatapp-client-usha.netlify.app">
             Capstone - chatapp with socket.io
-          </button>
+          </a>
           <p>•</p>
-          <button className="links" variant="text" href="https://weatherapinormal.netlify.app/">
+          <a className="links" variant="text" href="https://weatherapinormal.netlify.app/">
             Weather API
-          </button>
+          </a>
           <p>•</p>
-          <button className="links" variant="text" href="https://restcountriesweather.netlify.app">
+          <a className="links" variant="text" href="https://restcountriesweather.netlify.app">
             Rest countries API
-          </button>
+          </a>
           <p>•</p>
-          <button className="links" variant="text" href="https://formsandtables.netlify.app">
+          <a className="links" variant="text" href="https://formsandtables.netlify.app">
             Forms and tables
-          </button>
+          </a>
           <p>•</p>
-          <button className="links" variant="text" href="https://cat-apiusha.netlify.app">
+          <a className="links" variant="text" href="https://cat-apiusha.netlify.app">
             Cat API
-          </button>
+          </a>
           <p>•</p>
-          <button className="links" variant="text" href="https://dog-api-usha.netlify.app">
+          <a className="links" variant="text" href="https://dog-api-usha.netlify.app">
             Dog API
-          </button>
+          </a>
     </div>
     </div>
   )
