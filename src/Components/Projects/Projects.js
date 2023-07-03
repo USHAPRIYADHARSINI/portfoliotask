92194e4c9fa9dd1c11604adc206f10cd36b7f302
USHAPRIYADHARSINI <110903183+USHAPRIYADHARSINI@users.noreply.github.com>
2023-07-03 22:58:15 +0530

    updated projects

--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -6,12 +6,28 @@ function Projects() {
     <div className='projects'>
         <h3>Project Links</h3>
         <div className='projects-container'>
-          <a className="links" variant="text" href="https://spectacular-stroopwafel-e2df72.netlify.app/">
-          webcode 1 - Makeup api
+        <a className="links" variant="text" href="https://master--voluble-cannoli-688d68.netlify.app/">
+          Dashboard app
           </a>
           <p>•</p>
-          <a className="links" variant="text" href="https://chatapp-client-usha.netlify.app">
-            Capstone - chatapp with socket.io
+          <a className="links" variant="text" href="https://bookstore-react-node-usha.netlify.app/">
+          Online book store app
+          </a>
+          <p>•</p>
+          <a className="links" variant="text" href="https://chatapp-client-usha.netlify.app/">
+          Capstone - chatapp
+          </a>
+          <p>•</p>
+          <a className="links" variant="text" href="https://splendorous-pie-329b94.netlify.app/">
+          Student-teacher app
+          </a>
+          <p>•</p>
+          <a className="links" variant="text" href="https://stockprice-usha.netlify.app/">
+          Stockprice app
+          </a>
+          <p>•</p>
+          <a className="links" variant="text" href="https://spectacular-stroopwafel-e2df72.netlify.app/">
+          webcode 1 - Makeup api
           </a>
           <p>•</p>
           <a className="links" variant="text" href="https://weatherapinormal.netlify.app/">
